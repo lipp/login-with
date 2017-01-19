4978e9721f34bcc1f2571dbffacbd7f96a3263c3
Gerhard Preuss <gelipp@gmail.com>
2017-01-19 22:22:38 +0100

    add with-profile

---
+++ example/nextjs/with-profile.js
@@ -0,0 +1,41 @@
+import cookie from 'cookie'
+import Cookies from 'js-cookie'
+import React from 'react'
+
+export default ({Component, autoRedirect = false}) => (
+  class WithProfile extends React.Component {
+
+    static async getInitialProps (args) {
+      const {req, res} = args
+      let profile
+      if (req) {
+        const cookies = cookie.parse(req.headers.cookie || '')
+        profile = cookies.profile
+      } else {
+        profile = Cookies.get('profile')
+      }
+      try {
+        profile = JSON.parse(profile)
+        const props = Component.getInitialProps ? await Component.getInitialProps({...args, profile}) : {}
+        return {
+          ...props,
+          profile
+        }
+      } catch (_) {
+        if (autoRedirect) {
+          if (req) {
+            res.writeHead(302, { Location: '/login' })
+          } else {
+            document.location.pathname = '/login'
+          }
+        } else {
+          return Component.getInitialProps ? await Component.getInitialProps({...args}) : {}
+        }
+      }
+    }
+
+    render () {
+      return <Component {...this.props} />
+    }
+  }
+)
