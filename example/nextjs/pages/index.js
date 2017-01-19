8022382aa19ed56e5f35e6f7f775a1b6526f26fb
Gerhard Preuss <gelipp@gmail.com>
2017-01-19 12:15:44 +0100

    add basic example

--- example/nextjs/pages/index.js
+++ example/nextjs/pages/index.js
@@ -1,6 +1,29 @@
 import React from 'react'
 import cookie from 'cookie'
 
+const Login = ({service, redirect}) => (
+  <div className={'login ' + service.toLowerCase()} >
+    <a href={`https://login.now.sh/${service.toLowerCase()}?successRedirect=${redirect}&failureRedirect=${redirect}`} >
+      Login with {service}
+    </a>
+  </div>
+)
+
+const Logout = ({redirect}) => (
+  <div className='logout' >
+    <a href={`https://login.now.sh/logout?successRedirect=${redirect}`} >
+      Logout
+    </a>
+  </div>
+)
+
+const Profile = ({profile}) => (
+  <div className='profile'>
+    <h1>Hello {profile.displayName}</h1>
+    <img src={profile.photo} />
+    <h2>{profile.name.givenName} {profile.name.familyName}</h2>
+  </div>
+)
 
 export default class App extends React.Component {
   static getInitialProps ({req}) {
@@ -8,10 +31,9 @@ export default class App extends React.Component {
       try {
         const cookies = cookie.parse(req.headers.cookie || '')
         const profile = cookies.profile
-        const token = cookies.token
-        return {profile: JSON.parse(profile), token}
-      } catch (err) {
-        return {err}
+        return {profile: JSON.parse(profile)}
+      } catch (error) {
+        return {error}
       }
     }
   }
@@ -21,23 +43,20 @@ export default class App extends React.Component {
   }
 
   componentDidMount () {
-    console.log('props', this.props)
     this.setState({
       location: window.location.href
     })
   }
 
   render () {
-    const loginUrl = 'https://login.now.sh'
-    const {profile, token} = this.props
-    const {location} = this.state
+    const redirect = encodeURIComponent(this.state.location)
+    const {profile} = this.props
     return (
       <div>
-        { profile && <div><h1>{profile.displayName}</h1><img src={profile.photo} /></div> }
-        { token && <h1>{token}</h1> }
-        { location && !profile && <a href={`${loginUrl}/twitter?successRedirect=${encodeURIComponent(location)}`} >Login with Twitter</a> }
-        { location && !profile && <a href={`${loginUrl}/github?successRedirect=${encodeURIComponent(location)}`} >Login with GitHub</a> }
-        { location && profile && <a href={`${loginUrl}/logout?successRedirect=${encodeURIComponent(location)}`} >Log Out</a> }
+        { profile && <Profile profile={profile} /> }
+        { !profile && <Login service='Twitter' redirect={redirect} /> }
+        { !profile && <Login service='GitHub' redirect={redirect} /> }
+        { profile && <Logout redirect={redirect} /> }
       </div>
     )
   }
