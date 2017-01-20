e0af9942ffb31f2a6b731cfe0cb9ca96c950a8bc
Gerhard Preuss <gelipp@gmail.com>
2017-01-20 14:18:32 +0100

    use micro

--- example/token-decrypter/index.js
+++ example/token-decrypter/index.js
@@ -1,38 +1,22 @@
-const express = require('express')
-const cookieParser = require('cookie-parser')
 const jwt = require('jsonwebtoken')
-const url = require('url')
+const {send} = require('micro')
 const tokenSecret = process.env.LW_TOKEN_SECRET
+const cookie = require('cookie')
+const url = require('url')
 
 if (!tokenSecret) {
   console.error('no LW_TOKEN_SECRET env variable specified')
   process.exit(1)
 }
 
-const app = express()
-app.use(cookieParser())
-app.get('/', (req, res) => {
-  try {
-    const token = req.cookies.token
-    if (req.headers.referer) {
-      const {protocol, hostname} = url.parse(req.headers.referer)
-      const origin = `${protocol}//${hostname}`
-      res.header('Access-Control-Allow-Origin', origin)
-      res.header('Access-Control-Allow-Credentials', 'true')
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    }
-    if (!token) {
-      return res.json({error: 'no token cookie'})
-    }
-    try {
-      const decrypted = jwt.verify(token, tokenSecret)
-      return res.json({token, decrypted})
-    } catch (error) {
-      res.json({error})
-    }
-  } catch (error) {
-    res.json({error})
+module.exports = async (req, res) => {
+  const {token} = cookie.parse(req.headers.cookie)
+  if (req.headers.referer) {
+    const {protocol, hostname} = url.parse(req.headers.referer)
+    const origin = `${protocol}//${hostname}`
+    res.setHeader('Access-Control-Allow-Origin', origin)
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   }
-})
-
-app.listen(3000)
+  send(res, 200, {token, decrypted: jwt.verify(token, tokenSecret)})
+}
