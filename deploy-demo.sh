a800297aaa60cf709849bb40380981181a377ce5
Gerhard Preuss <gelipp@gmail.com>
2017-07-06 20:20:10 +0200

    move env config from now.json to deploy-demo.sh

---
+++ deploy-demo.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+now -e LW_SUBDOMAIN=auth.login-with.com -e LW_JWT_SECRET=@lw-token-secret -e LW_SESSION_SECRET=@lw-session-secret\
+	-e LW_REDDIT_CLIENTID=@lw-reddit-clientid -e LW_REDDIT_CLIENTSECRET=@lw-reddit-clientsecret \
+	-e LW_GITHUB_CLIENTID=@lw-github-clientid -e LW_GITHUB_CLIENTSECRET=@lw-github-clientsecret \
+	-e LW_TWITTER_CONSUMERKEY=@lw-twitter-consumerkey -e LW_TWITTER_CONSUMERSECRET=@lw-twitter-consumersecret \
+	-e LW_FACEBOOK_APPID=@lw-facebook-appid -e LW_FACEBOOK_APPSECRET=@lw-facebook-appsecret \
+	-e LW_GOOGLE_CLIENTID=@lw-google-clientid -e LW_GOOGLE_CLIENTSECRET=@lw-google-clientsecret && now alias auth.login-with.com
