3eb5d85f60387575024112d31e3971e719cc9a77
Juan Camilo Guarin Peñaranda <jcguarinpenaranda@gmail.com>
2017-07-06 02:12:42 -0500

    Ability to be required as an npm module (#27)
    * Created index file
    
    Index file exports the app, to allow this to be required as an npm module 
    and installed on a server already running on express
    
    * Now using exported app from index.js
    
    * added main file, and set port to read env PORT (optional, first priority for port)

--- server.js
+++ server.js
@@ -1,70 +1,11 @@
 const express = require('express')
-const passport = require('passport')
-const cookieParser = require('cookie-parser')
-const expressSession = require('express-session')
-const MemoryStore = require('session-memory-store')(expressSession)
-
+const LoginApp = require('./index')
 const opts = require('./src/opts')(process.argv, process.env)
 
-if (!opts.tokenSecret) {
-  console.error('no LW_JWT_SECRET env variable specified')
-  process.exit(1)
-}
+let app = express()
 
-if (!opts.sessionSecret) {
-  console.error('no LW_SESSION_SECRET env variable specified')
-  process.exit(1)
-}
+// use the LoginApp at '/'
+app.use('/', LoginApp)
 
-const rootUrl = opts.protocol + '/' + opts.subDomain
-
-console.log(`Using subdomain "${rootUrl}" for callback urls`)
-
-const strategies = require('./src/strategies')(process.env, rootUrl)
-console.log(`Configured strategies: ${strategies.map(strategy => strategy.type).join('/')}`)
-
-strategies.forEach(strategy => {
-  passport.use(new strategy.Ctor(strategy.config, strategy.toUser))
-  console.log(`Using login with "${strategy.type}" strategy`)
-})
-
-passport.serializeUser((user, done) => done(null, user))
-passport.deserializeUser((user, done) => done(null, user))
-
-const app = express()
-app.use(cookieParser())
-app.use(expressSession({
-  secret: opts.sessionSecret,
-  resave: false,
-  saveUninitialized: false,
-  store: new MemoryStore()
-}))
-app.use(passport.initialize())
-
-if (strategies.length > 0) {
-  opts.strategies = strategies
-  opts.passport = passport
-  const routes = require('./src/routes')(opts)
-
-  app.get(
-    strategies.map(strategy => `/${strategy.type}`),
-    routes.onAuthenticationRequest
-  )
-
-  app.get(
-    strategies.map(strategy => `/${strategy.type}/callback`),
-    routes.onAuthenticationCallback
-  )
-
-  app.get(
-    '/logout',
-    routes.onLogout
-  )
-
-  app.get(
-    '/',
-    routes.onIndex
-  )
-}
-
+// start the server
 app.listen(opts.port)
