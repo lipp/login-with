d7a346b6e08fa435c5072121b9ac9c2239cfb3c5
Gerhard Preuss <gelipp@gmail.com>
2017-07-07 17:13:02 +0200

    fix docker publish

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@ np
 if [ $? -eq 0 ]
 then
 	version=$(git describe --abbrev=0 --tags | cut -c2-)
-	docker build -t lipp/login-with:$version -i lipp/login-with:latest . && docker push lipp/login-with
+	docker build -t lipp/login-with:$version -t lipp/login-with:latest . && docker push lipp/login-with
 else
 	exit
 fi
