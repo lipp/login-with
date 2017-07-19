12d6d903981361814d677dc7bcfec0cf6304c1e3
Gerhard Preuss <gelipp@gmail.com>
2017-07-19 22:50:30 +0200

    add instagram env vars

--- deploy-demo.sh
+++ deploy-demo.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
-now -e LW_SUBDOMAIN=auth.login-with.com -e LW_JWT_SECRET=@lw-token-secret -e LW_SESSION_SECRET=@lw-session-secret\
+now -e LW_SUBDOMAIN=auth.login-with.com -e LW_JWT_SECRET=@lw-token-secret -e LW_SESSION_SECRET=@lw-session-secret \
 	-e LW_REDDIT_CLIENTID=@lw-reddit-clientid -e LW_REDDIT_CLIENTSECRET=@lw-reddit-clientsecret \
 	-e LW_LINKEDIN_CLIENTID=@lw-linkedin-clientid -e LW_LINKEDIN_CLIENTSECRET=@lw-linkedin-clientsecret \
+	-e LW_INSTAGRAM_CLIENTID=@lw-instagram-clientid -e LW_INSTAGRAM_CLIENTSECRET=@lw-instagram-clientsecret \
 	-e LW_GITHUB_CLIENTID=@lw-github-clientid -e LW_GITHUB_CLIENTSECRET=@lw-github-clientsecret \
 	-e LW_TWITTER_CONSUMERKEY=@lw-twitter-consumerkey -e LW_TWITTER_CONSUMERSECRET=@lw-twitter-consumersecret \
 	-e LW_FACEBOOK_APPID=@lw-facebook-appid -e LW_FACEBOOK_APPSECRET=@lw-facebook-appsecret \
