4e27f754f4859455a2a0848f09309960ffac1944
Gerhard Preuss <gelipp@gmail.com>
2017-08-09 11:16:58 +0200

    Create PULL_REQUEST_TEMPLATE.m

---
+++ PULL_REQUEST_TEMPLATE.m
@@ -0,0 +1,10 @@
+
+Checklist for new Strategies / Platforms
+
+ - [ ] Create a new strategy file (e.g. like [this](https://github.com/lipp/login-with/blob/master/src/strategies/strava.js))
+ - [ ] Add tests
+ - Add it to the README.md:
+   - [ ] About
+   - [ ] `<Platform>` specific environment variables
+   - [ ] Endpoint
+ - [ ] Test locally with your account
