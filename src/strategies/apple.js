b776d50cfc3690121f11cb07161c45c57dc0736f
Slava Ganzin <slava.ganzin@gmail.com>
2020-05-13 23:07:03 +0300

    add docs; change key

--- src/strategies/apple.js
+++ src/strategies/apple.js
@@ -1,9 +1,9 @@
 module.exports = {
   Ctor: require('passport-apple'),
   getConfig: (env, callbackURL) => {
-    if (!env.LW_APPLE_CLIENTID || !env.LW_APPLE_TEAMID || !env.LW_APPLE_CALLBACK || !env.LW_APPLE_KEYID || !env.LW_APPLE_KEYLOCATION) return
+    if (!env.LW_APPLE_SERVICEID || !env.LW_APPLE_TEAMID || !env.LW_APPLE_CALLBACK || !env.LW_APPLE_KEYID || !env.LW_APPLE_KEYLOCATION) return
     return {
-        clientID: env.LW_APPLE_CLIENTID,
+        clientID: env.LW_APPLE_SERVICEID,
         teamID: env.LW_APPLE_TEAMID,
         callbackURL: env.LW_APPLE_CALLBACK,
         keyID: env.LW_APPLE_KEYID,
@@ -11,7 +11,6 @@ module.exports = {
     }
   },
   toUser: (accessToken, refreshToken, profile, done) => {
-    console.log(accessToken, refreshToken, profile)
     done(null, {
       accessToken,
       refreshToken,
